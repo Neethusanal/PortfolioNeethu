1dea824f9bd1b2e31079a52fe44b8490271c9e08
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2024-05-23 13:07:21 +0000

    updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  " I am  Passionate self-taught web developer dedicated to crafting seamless online experiences. Eager to learn and explore new technologies to enhance digital landscape";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Backend Development",
+  "Frontend Development",
+  " ",
+  " ",
+  " ",
 ];
 
 /**
